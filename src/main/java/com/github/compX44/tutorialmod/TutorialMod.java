3f221c25b05e9202b7ca9d37f92e1e1c598994b6
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 11:23:24 -0400

    Initial commit

---
+++ src/main/java/com/github/compX44/tutorialmod/TutorialMod.java
@@ -0,0 +1,8 @@
+package com.github.compX44.tutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TutorialMod.MODID)
+public class TutorialMod {
+    public static final String MODID = "tutorialmod";
+}
